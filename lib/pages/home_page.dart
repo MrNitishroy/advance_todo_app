d36310070f0bfa61a921496ec2ea908967b6c095
Nitish Kumar <79355266+MrNitishroy@users.noreply.github.com>
2023-05-22 12:53:50 +0530

    home page completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,8 @@
+import 'package:advance_todo_app/components/my_button.dart';
+import 'package:advance_todo_app/components/my_text_form_field.dart';
 import 'package:advance_todo_app/config/my_colors.dart';
 import 'package:advance_todo_app/controller/auth_controller.dart';
+import 'package:advance_todo_app/controller/todo_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -9,23 +12,77 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     AuthController authController = Get.put(AuthController());
+    TodoController todoController = Get.put(TodoController());
     return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(
-            onPressed: () {
-              authController.logOut();
-            },
-            icon: Icon(
-              Icons.logout,
-              color: whiteColor,
+        appBar: AppBar(
+          actions: [
+            IconButton(
+              onPressed: () {
+                authController.logOut();
+              },
+              icon: Icon(
+                Icons.logout,
+                color: whiteColor,
+              ),
             ),
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Text("Add Todo"),
+              SizedBox(height: 10),
+              MyTextFormField(
+                controller: todoController.title,
+                lable: "Add Todo",
+              ),
+              SizedBox(height: 20),
+              MyButton(
+                btnName: "Add Todo",
+                onPress: () {},
+              ),
+              SizedBox(height: 20),
+              const Row(
+                children: [
+                  Text("All Todo"),
+                ],
+              ),
+              SizedBox(height: 10),
+              Expanded(
+                  child: Column(
+                children: [
+                  ListTile(
+                    tileColor: whiteColor,
+                    onTap: () {},
+                    leading: Icon(Icons.task_alt_outlined),
+                    title: Text(
+                      "Coocking and Baking",
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
+                    ),
+                    trailing: InkWell(
+                      child: Icon(Icons.delete),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  ListTile(
+                    tileColor: whiteColor,
+                    onTap: () {},
+                    leading: Icon(Icons.task_alt_outlined),
+                    title: Text(
+                      "Coocking and Baking",
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
+                    ),
+                    trailing: InkWell(
+                      child: Icon(Icons.delete),
+                    ),
+                  ),
+                ],
+              )),
+            ],
           ),
-        ],
-      ),
-      body: Center(
-        child: Text("Home page"),
-      ),
-    );
+        ));
   }
 }
